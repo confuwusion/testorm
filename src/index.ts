5bc4d1c5d2ef471c87aa4a786fc0d2f0579e1ee8
Shark Boi <immortalwarrior101@gmail.com>
2020-07-23 17:15:04 +0400

    Transfer orm configs to main file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import {LogChannel} from "./structs/db/entities/LogChannels";
-import {createConnection, getConnectionOptions, Connection} from "typeorm";
+import {Connection, createConnection, getConnectionOptions} from "typeorm";
 import {Subcommand} from "./structs/db/entities/Subcommands";
 import {CacheProvider} from "./structs/db/CacheProvider";
 import {Benchmark} from "./Benchmark";
@@ -24,6 +24,42 @@ console.log(`2 squared is`, powTwo(2));
 
   // const { cache } = await initiateManager();
 
+  const opts = [ {
+    name: `main`,
+    type: `sqlite`,
+    database: `./data/main.sql`,
+    synchronize: true,
+    logging: false,
+    cache: true,
+    migrations: [
+      `src/migration/**/*.ts`
+    ],
+    subscribers: [
+      `src/subscriber/**/*.ts`
+    ],
+    cli: {
+      migrationsDir: `src/migration`,
+      subscribersDir: `src/subscriber`
+    }
+  }, {
+    name: `cache`,
+    type: `sqlite`,
+    database: `:memory:`,
+    synchronize: true,
+    logging: false,
+    cache: true,
+    migrations: [
+      `src/migration/**/*.ts`
+    ],
+    subscribers: [
+      `src/subscriber/**/*.ts`
+    ],
+    cli: {
+      migrationsDir: `src/migration`,
+      subscribersDir: `src/subscriber`
+    }
+  } ];
+
   const connectionOptions = {
     cache: {
       duration: 0
@@ -38,11 +74,11 @@ console.log(`2 squared is`, powTwo(2));
   };
 
   const mainOptions = Object.assign(
-    await getConnectionOptions(`main`),
+    opts[0],
     connectionOptions
   );
 
-  // CONNECTIONS
+  // CONNECTIONSi
   const mainConnection = await createConnection(mainOptions);
 
 
@@ -57,49 +93,49 @@ console.log(`2 squared is`, powTwo(2));
   // await logRepo.clear();
   for (let i = 0; i < 2; ++i) {
 
-  console.log(`Insert bruh LogChannel`, lBr);
-  const insertBench = new Benchmark(true);
-  await logRepo.save([ modChannel ]);
-  insertBench
-    .stop()
-    .display(`Insertion took:`);
+    console.log(`Insert bruh LogChannel`, lBr);
+    const insertBench = new Benchmark(true);
+    await logRepo.save([ modChannel ]);
+    insertBench
+      .stop()
+      .display(`Insertion took:`);
 
 
-  console.log(`Get all LogChannels`, lBr);
-  const allBench = new Benchmark(true);
-  const allEntries = await logRepo.find();
-  allBench
-    .stop()
-    .display(`Find all took:`);
-  console.log(allEntries);
+    console.log(`Get all LogChannels`, lBr);
+    const allBench = new Benchmark(true);
+    const allEntries = await logRepo.find();
+    allBench
+      .stop()
+      .display(`Find all took:`);
+    console.log(allEntries);
 
-  console.log(`Update bruh LogChannel`, lBr);
-  const updateBench = new Benchmark(true);
-  await logRepo.update(
-    { type: `bruh` },
-    { channelID: `48848383` }
-  );
-  updateBench
-    .stop()
-    .display(`Update bruh took:`)
+    console.log(`Update bruh LogChannel`, lBr);
+    const updateBench = new Benchmark(true);
+    await logRepo.update(
+      { type: `bruh` },
+      { channelID: `48848383` }
+    );
+    updateBench
+      .stop()
+      .display(`Update bruh took:`);
 
-  console.log(`Get bruh LogChannel`, lBr);
-  const bruhBench = new Benchmark(true);
-  const bruhLog = await logRepo.findOne({
-    where: { type: `bruh` },
-    cache: true
-  });
-  bruhBench
-    .stop()
-    .display(`Find bruh took:`);
-  console.log(bruhLog);
+    console.log(`Get bruh LogChannel`, lBr);
+    const bruhBench = new Benchmark(true);
+    const bruhLog = await logRepo.findOne({
+      where: { type: `bruh` },
+      cache: true
+    });
+    bruhBench
+      .stop()
+      .display(`Find bruh took:`);
+    console.log(bruhLog);
 
-  console.log(`Delete bruh LogChannel`, lBr);
-  const deleteBench = new Benchmark(true);
-  await logRepo.delete({ type: `bruh` })
-  deleteBench
-    .stop()
-    .display(`Delete bruh took:`);
+    console.log(`Delete bruh LogChannel`, lBr);
+    const deleteBench = new Benchmark(true);
+    await logRepo.delete({ type: `bruh` });
+    deleteBench
+      .stop()
+      .display(`Delete bruh took:`);
 
   }
 })();
